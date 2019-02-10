d0f8372f1e7bee903e2fe76fa281a990ff8c9ba2
hemory <hphife@yahoo.com>
2019-02-10 09:04:52 -0500

    HomeWork 1 Part 1

---
+++ BasePlusCommissionEmployee.java
@@ -0,0 +1,101 @@
+package edu.brandeis.rseg102.oop;
+
+public class BasePlusCommissionEmployee
+{
+    private double baseSalary; // base salary per week
+    private CommissionEmployee commissionEmployee;
+
+    // six-argument constructor
+    public BasePlusCommissionEmployee( String firstName, String lastName,
+                                       String socialSecurityNumber, double grossSales, double commissionRate, double salary )
+    {
+        commissionEmployee = new CommissionEmployee( firstName, lastName, socialSecurityNumber, grossSales, commissionRate );
+        setBaseSalary( salary );
+    }
+
+    // set first name
+    public void setFirstName( String first )
+    {
+        commissionEmployee.setFirstName( first );
+    }
+
+    // return first nam
+    public String getFirstName()
+    {
+        return commissionEmployee.getFirstName();
+    }
+
+    // set last name
+    public void setLastName( String last )
+    {
+        commissionEmployee.setLastName( last );
+    }
+
+    // return last name
+    public String getLastName()
+    {
+        return commissionEmployee.getLastName();
+    }
+
+    // set social security number
+    public void setSocialSecurityNumber( String ssn )
+    {
+        commissionEmployee.setSocialSecurityNumber( ssn );
+    }
+
+    // return social security number
+    public String getSocialSecurityNumber()
+    {
+        return commissionEmployee.getSocialSecurityNumber();
+    }
+
+    // set commission employee's gross sales amount
+    public void setGrossSales( double sales )
+    {
+        commissionEmployee.setGrossSales( sales );
+    }
+
+    // return commission employee's gross sales amount
+    public double getGrossSales()
+    {
+        return commissionEmployee.getGrossSales();
+    }
+
+    // set commission employee's rate
+    public void setCommissionRate( double rate )
+    {
+        commissionEmployee.setCommissionRate( rate );
+    }
+
+    // return commission employee's rate
+    public double getCommissionRate()
+    {
+        return commissionEmployee.getCommissionRate();
+    }
+
+    // set base-salaried commission employee's base salary
+    public void setBaseSalary( double salary )
+    {
+        baseSalary = ( salary < 0.0 ) ? 0.0 : salary;
+    }
+
+    // return base-salaried commission employee's base salary
+    public double getBaseSalary()
+    {
+        return baseSalary;
+    }
+
+    // calculate base-salaried commission employee's earnings
+    public double earnings()
+    {
+        return getBaseSalary() + commissionEmployee.earnings();
+    }
+
+    // return String representation of BasePlusCommissionEmployee
+    @Override
+    public String toString()
+    {
+        return String.format( "%s %s\n%s: %.2f", "base-salaried",
+                commissionEmployee.toString(), "base salary", getBaseSalary() );
+    } // end method toString
+}
